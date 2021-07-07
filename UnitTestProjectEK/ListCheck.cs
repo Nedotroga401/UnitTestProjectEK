7e4b45c6ed40bc297334218a79710a3d6acf25ca
Nedotroga401 <Kyznetsova91@gmail.com>
2021-07-07 15:28:57 +0300

    New check

--- UnitTestProjectEK/ListCheck.cs
+++ UnitTestProjectEK/ListCheck.cs
@@ -39,13 +39,8 @@ namespace UnitTestProjectEK
             driver.FindElement(By.Name("username")).SendKeys("admin");
             driver.FindElement(By.Name("password")).SendKeys("admin");
             driver.FindElement(By.Name("login")).Click();
-            
-            
-
-
-          //IList<IWebElement> allButtons = driver.FindElements(By.PartialLinkText("app"));
+          
           IList<IWebElement> allButtons = driver.FindElements(By.Id("app-"));
-           
 
             int count = allButtons.Count;
             
@@ -54,13 +49,20 @@ namespace UnitTestProjectEK
                 WebDriverWait wait2 = new WebDriverWait(driver, TimeSpan.FromSeconds(3));
                 IWebElement element = wait2.Until(ExpectedConditions.ElementExists(By.Id("app-")));
                 IList<IWebElement> allButtonss = driver.FindElements(By.Id("app-"));
+
                 if (allButtonss[i] != null && allButtonss[i].Displayed && allButtonss[i].Enabled)
                     allButtonss[i].Click();
-                
+
+                if (driver.FindElement(By.TagName("h1"))!= null)
+                {
+                    Console.WriteLine("Заголовок есть");
+                }
+                else
+                {
+                    Console.WriteLine("Заголовка нет");
+                }
             }
         }
-
-        
 
         [TearDown]
         public void Stop()
